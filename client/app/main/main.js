1cea96c0e6e26f64a23f73fc9cb4a155812dd2bd
Logvin Oleg <oleg.logvin@faceit.com.ua>
2016-03-23 10:00:48 +0200

    route fix

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('jagenApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('main', {
-        url: '/',
-        template: '<main></main>'
-      });
-  });
+.config(function ($stateProvider) {
+  $stateProvider
+  .state('main', {
+    url: '/',
+    template: `
+      <navbar></navbar>
+      <main></main>
+      <footer></footer>
+    `
+  })
+});
